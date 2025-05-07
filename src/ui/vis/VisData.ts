0a66774523e301f4ad1de2777b1117e3351aec33
ahtoms <ahto@artifact8.xyz>
2025-05-07 17:35:17 +1000

    CHORE: adding vis data file

---
+++ src/ui/vis/VisData.ts
@@ -0,0 +1,14 @@
+
+
+
+export default class VisData {
+  static empty() {
+    return {
+      "width": 0,
+      "height" : 0,
+      "regions": [],
+      "layers" : [],
+      "base_layer": [],
+    }
+  }
+}
